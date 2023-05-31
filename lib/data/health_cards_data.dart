030c7adfd7b4bdcaac3c4ad5fd05600ae7867e4f
Yazan Eskief <yazaniskef@gmail.com>
2023-06-01 00:55:14 +0200

    create health card model and data for them

---
+++ lib/data/health_cards_data.dart
@@ -0,0 +1,125 @@
+import 'package:desmokrizer/models/card_health.dart';
+
+final List<CardHealth> cards = [
+  const CardHealth(
+    cardName: "Pulse rate",
+    shortDescription: "Your pulse rate should have returned to normal",
+    lonbgDescription:
+        "Quitting smoking improves pulse rate by allowing the heart to work more efficiently. When individuals stop smoking, their blood vessels relax, enabling better blood flow. As a result, the heart doesn't have to pump as hard, leading to a healthier and more stable pulse rate. Quitting smoking is crucial for overall cardiovascular health.",
+    image: "/assets/images/pulse.png",
+  ),
+  const CardHealth(
+    cardName: "Oxygen levels",
+    shortDescription: "Your oxygen levels should have returned to normal",
+    lonbgDescription:
+        "Quitting smoking boosts oxygen levels by allowing the lungs to heal and function better. With improved lung capacity, the body receives an increased supply of oxygen, benefiting overall health. Quitting smoking is vital for restoring optimal oxygen levels.",
+    image: "/assets/images/oxygen.png",
+  ),
+  const CardHealth(
+    cardName: "Carbon monoxide level",
+    shortDescription:
+        "Your carbon monoxide from smoking will have been eliminated from your body",
+    lonbgDescription:
+        "When individuals quit smoking, their carbon monoxide levels significantly decrease. Smoking introduces carbon monoxide into the bloodstream, reducing the capacity of red blood cells to carry oxygen. However, quitting smoking allows the body to clear out carbon monoxide, improving oxygen transportation and overall health.",
+    image: "/assets/images/carbon.png",
+  ),
+  const CardHealth(
+    cardName: "Nicotine expelled from body",
+    shortDescription: "All nicotine should now have been expelled",
+    lonbgDescription:
+        "After quitting smoking, the body begins to eliminate nicotine. Through metabolic processes, nicotine is broken down and expelled from the body. As time passes, nicotine levels gradually decrease, leading to reduced cravings and withdrawal symptoms. Quitting smoking allows the body to rid itself of nicotine and promotes a healthier, nicotine-free lifestyle.",
+    image: "/assets/images/nicotine.png",
+  ),
+  const CardHealth(
+    cardName: "Taste and smell",
+    shortDescription:
+        "Your ability to taste and smell should be greatly improved",
+    lonbgDescription:
+        "Quitting smoking enhances the sense of taste and smell. Smoking can dull these senses over time, but when individuals quit, the taste buds and olfactory receptors start to recover. This leads to a heightened ability to savor flavors and enjoy the aromas of food, ultimately enriching the overall sensory experience.",
+    image: "/assets/images/nose.png",
+  ),
+  const CardHealth(
+    cardName: "Breathing",
+    shortDescription:
+        "Your bronchial tubes have begun to relax and your breathing should be easier",
+    lonbgDescription:
+        "Quitting smoking greatly improves breathing. Smoking damages the lungs and reduces their capacity, making it harder to breathe. However, when individuals quit smoking, their lung function gradually improves, allowing for easier and more efficient breathing. Quitting smoking is essential for restoring and maintaining optimal respiratory health.",
+    image: "/assets/images/breathing.png",
+  ),
+  const CardHealth(
+    cardName: "Energy levels",
+    shortDescription: "Your energy levels should have returned to normal",
+    lonbgDescription:
+        "After quitting smoking, energy levels experience a noticeable improvement. Smoking can cause fatigue and decrease overall stamina. However, when individuals quit smoking, their circulation improves, oxygen levels increase, and the body's energy production becomes more efficient. This results in higher energy levels and a greater sense of vitality.",
+    image: "/assets/images/energy.png",
+  ),
+  const CardHealth(
+    cardName: "Bad breath gone",
+    shortDescription: "Any Smoking-realted bad breath should now have gone",
+    lonbgDescription:
+        "Quitting smoking leads to a significant improvement in bad breath. Smoking can cause persistent bad breath due to the tobacco's strong odor and its impact on oral health. However, when individuals quit smoking, their breath gradually becomes fresher as the odor-causing chemicals dissipate and oral hygiene improves.",
+    image: "/assets/images/bad-breath.png",
+  ),
+  const CardHealth(
+    cardName: "Tooth staining",
+    shortDescription:
+        "Your tooth staining and calculus build-up from smoking should be reduced",
+    lonbgDescription:
+        "Quitting smoking results in a reduction in tooth staining. Smoking tobacco can lead to yellowing and discoloration of the teeth over time. However, when individuals quit smoking, the staining process slows down and the natural color of the teeth becomes more evident. ",
+    image: "/assets/images/tooth-staining.png",
+  ),
+  const CardHealth(
+    cardName: "Gums and teeth",
+    shortDescription:
+        "The blood circulation in your gums and teeth should be similar to that of a non-smoker",
+    lonbgDescription:
+        "Quitting smoking has a positive impact on gum and teeth health. Smoking increases the risk of gum disease and tooth loss due to reduced blood flow and compromised immune function. However, when individuals quit smoking, blood flow improves, reducing inflammation and promoting healthier gums and stronger teeth",
+    image: "/assets/images/gum-and-teeth.png",
+  ),
+  const CardHealth(
+    cardName: "Circulation",
+    shortDescription: "Your circulation should be greatly improved",
+    lonbgDescription:
+        "Quitting smoking significantly improves circulation. Smoking damages blood vessels and reduces their ability to deliver oxygen and nutrients throughout the body. However, when individuals quit smoking, blood vessels gradually heal and become more efficient, leading to improved circulation",
+    image: "/assets/images/circulation.png",
+  ),
+  const CardHealth(
+    cardName: "Gum texture",
+    shortDescription:
+        "The texture and color of your gums should be back to normal",
+    lonbgDescription:
+        "Quitting smoking leads to improved gum texture. Smoking can cause gum tissue to become inflamed and lose its healthy appearance. However, when individuals quit smoking, the gums gradually heal, becoming firmer and less prone to inflammation.",
+    image: "/assets/images/gum-texture.png",
+  ),
+  const CardHealth(
+    cardName: "Immunity and lung function",
+    shortDescription: "Your immunity and lung function should be improved",
+    lonbgDescription:
+        "Quitting smoking results in improved immunity and lung function. Smoking weakens the immune system, making individuals more susceptible to infections and respiratory illnesses. However, when individuals quit smoking, their immune system strengthens, reducing the risk of illness. Additionally, lung function improves as the lungs heal and regain their capacity to efficiently process oxygen.",
+    image: "/assets/images/immunity.png",
+  ),
+  const CardHealth(
+    cardName: "Reduced rist of heart disease",
+    shortDescription:
+        "Your risk of heart disease has fallen to about half of that of a smoker",
+    lonbgDescription:
+        "Quitting smoking significantly decreases the risk of heart disease. Smoking damages blood vessels, increases blood pressure, and raises the risk of heart attacks and strokes. However, when individuals quit smoking, their cardiovascular health improves as blood vessels begin to heal and blood pressure normalizes, reducing the risk of heart disease. Quitting smoking is a vital step towards protecting and preserving heart health",
+    image: "/assets/images/heart-disease.png",
+  ),
+  const CardHealth(
+    cardName: "Decreased risk of lung cancer",
+    shortDescription:
+        "Your risk of lung cancer has fallen to about half of that of smoker",
+    lonbgDescription:
+        "Quitting smoking dramatically reduces the risk of developing lung cancer. Smoking is the leading cause of lung cancer, with harmful chemicals in cigarettes damaging lung cells and increasing the likelihood of cancerous growth. However, when individuals quit smoking, their lung cancer risk gradually decreases, allowing the lungs to heal and reducing exposure to carcinogens.",
+    image: "/assets/images/lung-cancer.png",
+  ),
+  const CardHealth(
+    cardName: "Decreased risk of heart attack",
+    shortDescription:
+        "Your risk of heart attack has fallen to about half of that of a smoker",
+    lonbgDescription:
+        "Quitting smoking significantly reduces the risk of heart attack. Smoking damages blood vessels, increases blood pressure, and promotes the formation of blood clots, all of which contribute to heart attacks. However, when individuals quit smoking, their cardiovascular health improves, blood vessels heal, and the risk of heart attack decreases.",
+    image: "/assets/images/heart-attack.png",
+  ),
+];
---
+++ lib/models/card_health.dart
@@ -0,0 +1,13 @@
+class CardHealth {
+  const CardHealth({
+    required this.cardName,
+    required this.shortDescription,
+    required this.lonbgDescription,
+    required this.image,
+  });
+
+  final String cardName;
+  final String shortDescription;
+  final String lonbgDescription;
+  final String image;
+}
